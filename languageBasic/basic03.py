8e1b5c5438d5300f5a7d8097d6fac1f1634b29f4
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2024-12-23 15:27:39 +0900

    파이썬 slicing 기본

--- languageBasic/basic03.py
+++ languageBasic/basic03.py
@@ -6,11 +6,4 @@ print(s.strip())  # "Hello World"
 ss = "--Hel--lo--"
 print(ss.strip('-'))  # "Hel--lo"
 
-price = '1,234원'
-print(price[:-1]) # 1,234   문자열의 처음부터 마지막 문자 바로 앞까지를 선택, 마지막 문자를 제외한 모든 부분을 가져옴
-print(price[-1]) # 원       마지막 문자
-print(price[1:]) # ,234원   첫 번째 문자부터 끝까지
-# price[start:end]
-# start: 시작 인덱스 (생략하면 0부터 시작)
-# end: 끝 인덱스 (생략하면 문자열 끝까지)
-# 음수 인덱스는 뒤에서부터 센 위치를 의미.
+
--- selenium/crawling/dhLottery/lotteryCrwl01.py
+++ selenium/crawling/dhLottery/lotteryCrwl01.py
@@ -20,6 +20,6 @@ print(bonus)
 # (session="389b18b1dfbd10b3166b953a65076cd3"
 # , element="f.A42F4E2555CF2758C585963E6FC5B863.d.E695279D5F0206D6DB4050265252582B.e.120")>
 
-#### num_view.find_elements(By.XPATH, "*") 결과물
+#### num_comp = num_view.find_elements(By.XPATH, "*") 결과물
 # [ ] 안에 위와 똑같은 형태로 여러개 들어있음
 # <selenium.webdriver.remote.webelement.WebElement (session="389b18b1dfbd10b3166b953a65076cd3", element="f.A42F4E2555CF2758C585963E6FC5B863.d.E695279D5F0206D6DB4050265252582B.e.175")>]

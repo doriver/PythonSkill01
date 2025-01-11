3ab2d60bf0c4af6fbe731cdff4dcc8dc7ae3c2d6
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-01-12 00:19:35 +0900

    프로젝트 정리

--- basic/languageBasic/forBasic01.py
+++ basic/languageBasic/forBasic01.py
@@ -1,3 +1,12 @@
+for i in range(3): # 0, 1, 2
+    print(i)
+
+
+for i in range(2, 5): # 2, 3, 4
+    print(i)
+
+# for문 range(start, stop, step) 끝값은 포함x
+
 for i in range(5):
     if i == 2:
         continue # 현재 반복을 건너뛰고 다음 반복으로 넘어감
